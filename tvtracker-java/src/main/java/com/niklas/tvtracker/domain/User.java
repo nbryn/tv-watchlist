42630611dbc617ddfb3b684ef11097a1a5fcb946
nbryn <niklasbg@live.dk>
2018-12-15 17:55:59 +0100

    Started implemementation of security

--- tvtracker-java/src/main/java/com/niklas/tvtracker/domain/User.java
+++ tvtracker-java/src/main/java/com/niklas/tvtracker/domain/User.java
@@ -1,4 +1,136 @@
 package com.niklas.tvtracker.domain;
 
-public class User {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Collection;
+import java.util.Date;
+
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Username needs to be an email")
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Please enter your full name")
+    private String fullName;
+
+    @NotBlank(message = "Please enter a valid password")
+    private String password;
+
+    @Transient
+    private String confirmPassword;
+
+    private Date created_At;
+    private Date updated_At;
+
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public Date getCreate_At() {
+        return created_At;
+    }
+
+    public void setCreate_At(Date create_At) {
+        this.created_At = create_At;
+    }
+
+    public Date getUpdate_At() {
+        return updated_At;
+    }
+
+    public void setUpdate_At(Date update_At) {
+        this.updated_At = update_At;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_At = new Date();
+    }
+
+    @Override
+    @JsonIgnore
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isEnabled() {
+        return true;
+    }
 }
+
---
+++ tvtracker-java/src/main/java/com/niklas/tvtracker/services/UserService.java
@@ -0,0 +1,23 @@
+package com.niklas.tvtracker.services;
+
+import com.niklas.tvtracker.domain.User;
+import com.niklas.tvtracker.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userRepository.save(user);
+    }
+}
